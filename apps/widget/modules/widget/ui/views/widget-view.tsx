e74539af56f5980f9552c1679a76f1226e0d6141
Darrell Robinson <3dfordinner@gmail.com>
2025-12-20 15:17:00 -0500

    phase9: widget screen router

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -1,7 +1,7 @@
 "use client";
-// import {useForm} from "react-hook-form"
-// import {z} from "zod"
-// import {zodResovler} from "@hookform/resolvers/zod"
+import { useAtomValue } from "jotai";
+
+import { screenAtom } from "../../atoms/widget-atoms";
 import { WidgetAuthScreen } from "../screens/widget-auth-screen";
 
 interface Props {
@@ -9,10 +9,22 @@ interface Props {
 }
 
 export const WidgetView = ({ organizationId }: Props) => {
+  const screen = useAtomValue(screenAtom);
+
+  const screenComponents = {
+    error: <p>TODO: Error</p>,
+    loading: <p>TODO: Loading</p>,
+    auth: <WidgetAuthScreen />,
+    voice: <p>TODO: Voice</p>,
+    inbox: <p>TODO: Inbox</p>,
+    selection: <p>TODO: Selection</p>,
+    chat: <p>TODO: Chat</p>,
+    contact: <p>TODO: Contact</p>,
+  };
+
   return (
     <main className="min-h-screen min-w-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
-      <WidgetAuthScreen />
-      {/* <WidgetFooter /> */}
+      {screenComponents[screen]}
     </main>
   );
 };
