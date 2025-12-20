398db3b3f9c2e1f9c3fbb8800bda549edaa25a36
Darrell Robinson <3dfordinner@gmail.com>
2025-12-20 15:21:38 -0500

    phase9

--- apps/widget/components/providers.tsx
+++ apps/widget/components/providers.tsx
@@ -1,10 +1,15 @@
 "use client";
 
 import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { Provider } from "jotai";
 import * as React from "react";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL || "");
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+  return (
+    <ConvexProvider client={convex}>
+      <Provider>{children}</Provider>
+    </ConvexProvider>
+  );
 }
