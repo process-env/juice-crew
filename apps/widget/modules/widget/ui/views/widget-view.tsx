e85fd35130eb6550b0919bef822cbf52b9d35950
Darrell Robinson <3dfordinner@gmail.com>
2025-12-20 13:16:56 -0500

    phase8: widget session

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -1,7 +1,8 @@
 "use client";
-
-import { WidgetFooter } from "../components/widget-footer";
-import { WidgetHeader } from "../components/widget-header";
+// import {useForm} from "react-hook-form"
+// import {z} from "zod"
+// import {zodResovler} from "@hookform/resolvers/zod"
+import { WidgetAuthScreen } from "../screens/widget-auth-screen";
 
 interface Props {
   organizationId: string;
@@ -10,12 +11,8 @@ interface Props {
 export const WidgetView = ({ organizationId }: Props) => {
   return (
     <main className="min-h-screen min-w-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
-      <WidgetHeader>
-        <p className="font-semibold text-3xl">Hi there!</p>
-        <p className="font-semibold text-lg">How can we help you today?</p>
-      </WidgetHeader>
-      <div className="flex flex-1">Widget View: {organizationId}</div>
-      <WidgetFooter />
+      <WidgetAuthScreen />
+      {/* <WidgetFooter /> */}
     </main>
   );
 };
